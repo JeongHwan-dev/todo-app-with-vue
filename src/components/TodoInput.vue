cd8602282ebff69b9a9066b2d45913d1e47d16ec
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-08 16:12:12 +0900

    Feat : Todo 입력 컴포넌트 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,65 @@
 <template>
-  <div>input</div>
+  <div class="input-box shadow">
+    <input
+      type="text"
+      class="input-box__input"
+      v-model="newTodoItem"
+      @keyup.enter="addTodo"
+    />
+    <span class="input-box__add-btn" @click="addTodo">
+      <FontAwesomeIcon icon="plus" class="input-box__add-btn-icon" />
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+    };
+  },
+  methods: {
+    clearInput() {
+      this.newTodoItem = '';
+    },
+    addTodo() {
+      if (this.newTodoItem !== '') {
+        const todoItemObject = { completed: false, item: this.newTodoItem };
+
+        localStorage.setItem(this.newTodoItem, JSON.stringify(todoItemObject));
+        this.clearInput();
+      }
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.input-box {
+  height: 50px;
+  border-radius: 5px;
+  background-color: #fff;
+  line-height: 50px;
+}
+.input-box .input-box__input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.input-box .input-box__input:focus {
+  outline: none;
+}
+
+.input-box__add-btn {
+  display: block;
+  width: 3rem;
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.input-box__add-btn-icon {
+  vertical-align: middle;
+  color: #fff;
+}
+</style>
