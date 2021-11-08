9323ac7578b04b18d069e57fa9b9d81c5e3e0954
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-08 16:10:04 +0900

    Set : 'Font Awesome Icon' 세팅

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUserSecret);
+library.add(fas);
 
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
 
