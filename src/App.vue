969a631441b367a3ee84d45a8bcb0ef922b63ccf
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-08 16:11:16 +0900

    Design : 공통 디자인 스크립트 추가

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  background-color: #f6f6f6;
+  text-align: center;
+}
+
+input {
+  width: 200px;
+  border-style: groove;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
