60f305408a7cda3657df89d2f2cf006232de1797
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-08 16:12:57 +0900

    Feat : Todo 푸터 컴포넌트 구현

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>footer</div>
+  <div class="clear-all-container">
+    <span class="clear-all-container__btn" @click="clearTodo">Clear All</span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    clearTodo() {
+      localStorage.clear();
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.clear-all-container {
+  width: 8.5rem;
+  height: 50px;
+  margin: 0 auto;
+  border-radius: 5px;
+  background-color: #fff;
+  line-height: 50px;
+}
+.clear-all-container__btn {
+  display: block;
+  color: #e20303;
+  cursor: pointer;
+}
+</style>
